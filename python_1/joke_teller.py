d9628bd8572d6a07dc31f5d2e189abf7f38ac62c
Eleni <djxzero5@gmail.com>
2025-07-09 13:21:32 -0400

    Created folders and organized them accordingly

---
+++ python_1/joke_teller.py
@@ -0,0 +1,17 @@
+import random
+
+# List of jokes
+jokes = [
+    "Why don't skeletons fight each other? They don't have the guts.",
+    "Why did the scarecrow win an award? Because he was outstanding in his field.",
+    "Why don’t eggs tell jokes? They’d crack each other up.",
+    "Why did the math book look sad? It had too many problems.",
+    "How do trees get on the Internet? They log in.'"
+]
+
+# Pick a random joke
+joke = random.choice(jokes)
+
+# Display it
+print("Here's a joke for you:")
+print(joke)
